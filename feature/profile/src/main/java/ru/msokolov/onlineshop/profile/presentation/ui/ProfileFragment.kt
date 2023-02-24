7e5b6c57d41ae6841b14cdc1553221190f85bc55
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-02-24 22:13:27 +0300

    initialized page_one module, implemented core:ui module

---
+++ feature/profile/src/main/java/ru/msokolov/onlineshop/profile/presentation/ui/ProfileFragment.kt
@@ -0,0 +1,8 @@
+package ru.msokolov.onlineshop.profile.presentation.ui
+
+import androidx.fragment.app.Fragment
+import ru.msokolov.onlineshop.profile.R
+
+class ProfileFragment : Fragment(R.layout.fragment_profile) {
+
+}
