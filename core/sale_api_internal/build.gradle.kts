4ad59201632c9b3a4964162e2ba7613ee744e449
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-02-26 00:10:47 +0300

    removed latest_api to core module, "apied" it to core:sale_api_internal, implemented retrofit and gson converter

--- core/sale_api_internal/build.gradle.kts
+++ core/sale_api_internal/build.gradle.kts
@@ -41,4 +41,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+    api(project(":core:sale_api"))
+
+    implementation(Network.retrofitCore)
+    implementation(Network.retrofitGson)
 }
