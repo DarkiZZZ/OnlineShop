0efbd7c1dc1bf14a4daa446e4cb8ee051f4e8d63
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-02-25 21:22:04 +0300

    added button navigation click listener(to ProfileFragment), added destination listener to manage bottom navigation visibility

--- app/src/main/java/ru/msokolov/onlineshop/MainActivity.kt
+++ app/src/main/java/ru/msokolov/onlineshop/MainActivity.kt
@@ -2,10 +2,51 @@ package ru.msokolov.onlineshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import ru.msokolov.onlineshop.bottom_navigation.BottomNavigation
+import ru.msokolov.onlineshop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    lateinit var bottomNavigation: BottomNavigation
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        binding.profileButton.setOnClickListener {
+            findNavController(binding.fragmentContainerView.id)
+                .navigate(bottomNavigation.toProfile.action)
+        }
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener{_, destination, _ ->
+            when(destination.id){
+                R.id.signInPageFragment -> { setBottomNavigationVisibility(false) }
+                R.id.loginFragment -> { setBottomNavigationVisibility(false) }
+                R.id.pageOneFragment -> { setBottomNavigationVisibility(true) }
+                R.id.pageTwoFragment -> { setBottomNavigationVisibility(true) }
+                R.id.profileButton -> { setBottomNavigationVisibility(true) }
+            }
+        }
+        super.onStart()
+    }
+
+    private fun setBottomNavigationVisibility(isVisible: Boolean){
+        if (isVisible){
+            binding.bottomNavigationView.visibility = View.VISIBLE
+        }
+        else{
+            binding.bottomNavigationView.visibility = View.GONE
+        }
     }
 }
