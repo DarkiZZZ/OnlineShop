33f1f4784fe01ff561152317e9a7e2111f0d1fc4
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-02-26 20:32:39 +0300

    filled fragment to test DI, VM and API

--- feature/page_one/src/main/java/ru/msokolov/onlineshop/page_one/presentation/ui/PageOneFragment.kt
+++ feature/page_one/src/main/java/ru/msokolov/onlineshop/page_one/presentation/ui/PageOneFragment.kt
@@ -1,10 +1,17 @@
 package ru.msokolov.onlineshop.page_one.presentation.ui
 
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import dagger.Lazy
+import ru.msokolov.onlineshop.dagger.findDependencies
 import ru.msokolov.onlineshop.page_one.R
 import ru.msokolov.onlineshop.page_one.databinding.FragmentPageOneBinding
+import ru.msokolov.onlineshop.page_one.di.DaggerPageOneComponent
 import javax.inject.Inject
 
 class PageOneFragment : Fragment(R.layout.fragment_page_one) {
@@ -15,4 +22,25 @@ class PageOneFragment : Fragment(R.layout.fragment_page_one) {
     lateinit var viewModelFactory : Lazy<PageOneViewModel.Companion.PageOneViewModelFactory>
 
     private val viewModel : PageOneViewModel by viewModels{ viewModelFactory.get() }
+
+    override fun onAttach(context: Context) {
+        DaggerPageOneComponent.builder()
+            .pageOneDependencies(findDependencies())
+            .build()
+            .inject(this)
+        super.onAttach(context)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        lifecycleScope.launchWhenStarted {
+            viewModel.getLatestData().collect{
+                when(it.status){
+                    Status.SUCCESS -> Log.d("TAGTAGTAG", it.data.toString())
+                    Status.ERROR -> Log.d("TAGTAGTAG", it.message.toString())
+                    Status.LOADING -> Log.d("TAGTAGTAG", it.message.toString())
+                }
+            }
+        }
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
