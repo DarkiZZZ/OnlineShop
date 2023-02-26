f6c22734d560a13781b95efad1828c0e5ac9f99b
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-02-26 18:49:26 +0300

    added dependencies (APIs) to PageOneComponent.kt

--- feature/page_one/src/main/java/ru/msokolov/onlineshop/page_one/di/PageOneComponent.kt
+++ feature/page_one/src/main/java/ru/msokolov/onlineshop/page_one/di/PageOneComponent.kt
@@ -2,7 +2,9 @@ package ru.msokolov.onlineshop.page_one.di
 
 import dagger.Component
 import ru.msokolov.onlineshop.dagger.Dependencies
+import ru.msokolov.onlineshop.latest_api.LatestApiService
 import ru.msokolov.onlineshop.page_one.presentation.ui.PageOneFragment
+import ru.msokolov.onlineshop.sale_api.SaleApiService
 
 @Component(modules = [PageOneModule::class], dependencies = [PageOneDependencies::class])
 internal interface PageOneComponent {
@@ -12,7 +14,11 @@ internal interface PageOneComponent {
     interface Builder{
 
         fun build():PageOneComponent
+        fun pageOneDependencies(dependencies: PageOneDependencies) : Builder
     }
 }
 
-interface PageOneDependencies: Dependencies
+interface PageOneDependencies: Dependencies{
+    val latestApiService: LatestApiService
+    val saleApiService: SaleApiService
+}
