2138cbdcfc9a62f01b4c7f484f79c4f419aade2f
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-02-26 18:47:25 +0300

    added @Inject to implementations of APIs repositories

--- feature/page_one/src/main/java/ru/msokolov/onlineshop/page_one/data/repository/latest/LatestApiRepositoryImpl.kt
+++ feature/page_one/src/main/java/ru/msokolov/onlineshop/page_one/data/repository/latest/LatestApiRepositoryImpl.kt
@@ -2,8 +2,9 @@ package ru.msokolov.onlineshop.page_one.data.repository.latest
 
 import ru.msokolov.onlineshop.latest_api.LatestApiService
 import ru.msokolov.onlineshop.latest_api.models.LatestResponseDto
+import javax.inject.Inject
 
-class LatestApiRepositoryImpl(private val apiService: LatestApiService): LatestApiRepository {
+class LatestApiRepositoryImpl @Inject constructor(private val apiService: LatestApiService): LatestApiRepository {
 
     override suspend fun getLatestResponseDto(): LatestResponseDto {
         return apiService.getLatestResponseDto()
--- feature/page_one/src/main/java/ru/msokolov/onlineshop/page_one/data/repository/sale/SaleApiRepositoryImpl.kt
+++ feature/page_one/src/main/java/ru/msokolov/onlineshop/page_one/data/repository/sale/SaleApiRepositoryImpl.kt
@@ -2,8 +2,9 @@ package ru.msokolov.onlineshop.page_one.data.repository.sale
 
 import ru.msokolov.onlineshop.sale_api.SaleApiService
 import ru.msokolov.onlineshop.sale_api.models.SaleResponseDto
+import javax.inject.Inject
 
-class SaleApiRepositoryImpl(private val apiService: SaleApiService): SaleApiRepository {
+class SaleApiRepositoryImpl @Inject constructor(private val apiService: SaleApiService): SaleApiRepository {
 
     override suspend fun getSaleResponseDto(): SaleResponseDto {
         return apiService.getSaleResponseDto()
