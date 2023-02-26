cf4ccc56aa3d5eefecbd5c177dc9ca74b4f0cd82
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-02-26 20:10:36 +0300

    added typealias

--- core/dagger/src/main/java/ru/msokolov/onlineshop/dagger/Dependencies.kt
+++ core/dagger/src/main/java/ru/msokolov/onlineshop/dagger/Dependencies.kt
@@ -2,6 +2,8 @@ package ru.msokolov.onlineshop.dagger
 
 interface Dependencies
 
+typealias DependenciesMap = Map<Class<out Dependencies>, @JvmSuppressWildcards Dependencies>
+
 interface HasDependencies{
-    val dependenciesMap: Map<Class<out Dependencies>, @JvmSuppressWildcards Dependencies>
+    val dependenciesMap: DependenciesMap
 }
