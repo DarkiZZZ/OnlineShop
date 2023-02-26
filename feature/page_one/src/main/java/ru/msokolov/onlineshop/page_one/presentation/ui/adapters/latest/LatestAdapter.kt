1da7c42120a05d2205fbbb844cd2cd19f5860b2e
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-02-27 00:01:46 +0300

    initialized adapters for PageOneFragment.kt

---
+++ feature/page_one/src/main/java/ru/msokolov/onlineshop/page_one/presentation/ui/adapters/latest/LatestAdapter.kt
@@ -0,0 +1,30 @@
+package ru.msokolov.onlineshop.page_one.presentation.ui.adapters.latest
+
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.msokolov.onlineshop.page_one.databinding.LatestItemBinding
+
+class LatestAdapter(private val context: Context) :
+    RecyclerView.Adapter<LatestAdapter.LatestViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LatestViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: LatestViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class LatestViewHolder(private val binding: LatestItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+            /*fun bind()*/
+        }
+
+
+}
