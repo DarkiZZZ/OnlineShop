09d307d0c774bbbf7f6fab8536891f74a753a076
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-02-28 20:03:55 +0300

    changed test code

--- feature/page_one/src/main/java/ru/msokolov/onlineshop/page_one/presentation/ui/PageOneFragment.kt
+++ feature/page_one/src/main/java/ru/msokolov/onlineshop/page_one/presentation/ui/PageOneFragment.kt
@@ -11,14 +11,13 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import dagger.Lazy
 import ru.msokolov.onlineshop.dagger.findDependencies
-import ru.msokolov.onlineshop.latest_api.models.LatestDto
-import ru.msokolov.onlineshop.latest_api.models.LatestResponseDto
 import ru.msokolov.onlineshop.navigation.navigate
 import ru.msokolov.onlineshop.page_one.R
+import ru.msokolov.onlineshop.page_one.data.entity.FlashSaleListEntity
+import ru.msokolov.onlineshop.page_one.data.entity.LatestListEntity
 import ru.msokolov.onlineshop.page_one.databinding.FragmentPageOneBinding
 import ru.msokolov.onlineshop.page_one.di.DaggerPageOneComponent
 import ru.msokolov.onlineshop.page_one.presentation.ui.adapters.latest.SaleAdapter
-import ru.msokolov.onlineshop.sale_api.models.SaleResponseDto
 import javax.inject.Inject
 
 class PageOneFragment : Fragment(R.layout.fragment_page_one) {
@@ -58,8 +57,8 @@ class PageOneFragment : Fragment(R.layout.fragment_page_one) {
                 when(it.status){
                     Status.SUCCESS -> {
                         when(it.data){
-                            is LatestResponseDto -> { Log.d("TAGTAGTAG", "latest: ${ it.data.latest }")}
-                            is SaleResponseDto -> {Log.d("TAGTAGTAG", "sale: ${ it.data.flashSale }")}
+                            is LatestListEntity -> { Log.d("TAGTAGTAG", "latest: ${ it.data.latestList }")}
+                            is FlashSaleListEntity -> {Log.d("TAGTAGTAG", "sale: ${ it.data.flashSaleList }")}
                         }
                     }
                     Status.ERROR -> Log.d("TAGTAGTAG", "error: ${it.message.toString()}")
