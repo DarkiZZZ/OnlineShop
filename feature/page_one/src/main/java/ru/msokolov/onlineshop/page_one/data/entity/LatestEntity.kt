5a07fdb77b8ae4ec2d2c357c27ad7e3ec9eb0950
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-02-28 22:43:20 +0300

    implemented delegate adapters, added them to fragment

--- feature/page_one/src/main/java/ru/msokolov/onlineshop/page_one/data/entity/LatestEntity.kt
+++ feature/page_one/src/main/java/ru/msokolov/onlineshop/page_one/data/entity/LatestEntity.kt
@@ -1,8 +1,47 @@
 package ru.msokolov.onlineshop.page_one.data.entity
 
+import ru.msokolov.onlineshop.page_one.presentation.ui.adapters.delegate.DelegateAdapterItem
+
 data class LatestEntity(
     val category : String,
     val name     : String,
     val price    : String,
     val imageUrl : String
-)
+): DelegateAdapterItem {
+
+    override fun id(): Any {
+        return name
+    }
+
+    override fun content(): Any {
+        return FlashSaleEntityContent(price = price)
+    }
+
+    override fun payload(other: Any): DelegateAdapterItem.Payloadable {
+        if (other is FlashSaleEntity) {
+            if (price != other.price) {
+                return ChangePayload.PriceChanged(other.price)
+            }
+        }
+        return DelegateAdapterItem.Payloadable.None
+    }
+
+    inner class FlashSaleEntityContent(val price: String) {
+        override fun equals(other: Any?): Boolean {
+            if (other is FlashSaleEntityContent) {
+                return price == other.price
+            }
+            return false
+        }
+
+        override fun hashCode(): Int {
+            var result = price.hashCode()
+            result = 31 * result + price.hashCode()
+            return result
+        }
+    }
+
+    sealed class ChangePayload : DelegateAdapterItem.Payloadable {
+        data class PriceChanged(val price: String) : ChangePayload()
+    }
+}
---
+++ feature/page_one/src/main/java/ru/msokolov/onlineshop/page_one/presentation/ui/adapters/delegate/latest/FlashSaleDelegateAdapter.kt
@@ -0,0 +1,50 @@
+package ru.msokolov.onlineshop.page_one.presentation.ui.adapters.delegate.latest
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import ru.msokolov.onlineshop.page_one.data.entity.FlashSaleEntity
+import ru.msokolov.onlineshop.page_one.databinding.FlashSaleItemBinding
+import ru.msokolov.onlineshop.page_one.presentation.ui.adapters.delegate.DelegateAdapter
+import ru.msokolov.onlineshop.page_one.presentation.ui.adapters.delegate.DelegateAdapterItem
+
+class FlashSaleDelegateAdapter(private val navigateToPageTwo: () -> Unit) :
+    DelegateAdapter<FlashSaleEntity, FlashSaleDelegateAdapter.FlashSaleViewHolder>(FlashSaleEntity::class.java) {
+
+    inner class FlashSaleViewHolder(private val binding: FlashSaleItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: FlashSaleEntity) {
+            binding.itemCostTextView.text = "$ ${item.price}"
+            binding.itemNameTextView.text = item.name
+            binding.categoryTextView.text = item.category
+            binding.discountTextView.text = "${item.discount}% off"
+            binding.backgroundImageView.setOnClickListener {
+                navigateToPageTwo()
+            }
+            Picasso.get()
+                .load(item.imageUrl)
+                .into(binding.backgroundImageView)
+        }
+    }
+
+    override fun createViewHolder(parent: ViewGroup): RecyclerView.ViewHolder {
+        val itemBinding =
+            FlashSaleItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return FlashSaleViewHolder(itemBinding)
+    }
+
+    override fun bindViewHolder(
+        model: FlashSaleEntity,
+        viewHolder: FlashSaleViewHolder,
+        payloads: List<DelegateAdapterItem.Payloadable>
+    ) {
+        when (val payload = payloads.firstOrNull() as? FlashSaleEntity.ChangePayload) {
+            is FlashSaleEntity.ChangePayload.PriceChanged -> {}
+            is FlashSaleEntity.ChangePayload.DiscountChanged -> {}
+
+            else -> viewHolder.bind(model)
+        }
+    }
+}
