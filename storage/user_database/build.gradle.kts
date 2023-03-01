27a38d70a0bb2c3c0273dfaab679488eba9d4a01
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-03-01 13:07:38 +0300

    added appDatabase, Dao, Db entity

--- storage/user_database/build.gradle.kts
+++ storage/user_database/build.gradle.kts
@@ -1,6 +1,9 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -35,7 +38,13 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
+    implementation(project(":storage:user_database_api"))
+
+    implementation(Room.roomRuntime)
+    implementation(Room.roomKtx)
+    kapt(Room.roomCompiler)
+
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.8.0")
     testImplementation("junit:junit:4.13.2")
