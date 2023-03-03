e745d36fb8f6174695684bff7aa51500a23bc1dc
Mike Sokolov <mark_sokolov_2016@mail.ru>
2023-03-03 22:07:27 +0300

    added extension method to work with Shared Prefs to Fragment

--- core/ui/src/main/java/ru/msokolov/onlineshop/ui/FragmentExtensions.kt
+++ core/ui/src/main/java/ru/msokolov/onlineshop/ui/FragmentExtensions.kt
@@ -7,12 +7,25 @@ import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 
-fun Fragment.showSnackBar(parentView: View, message: String){
+fun Fragment.showSnackBar(parentView: View, message: String) {
     Snackbar.make(parentView, message, Snackbar.LENGTH_SHORT).show()
 }
 
 fun Fragment.hideKeyboard() {
     view?.let { activity?.hideKeyboard(it) }
+}
+
+fun Fragment.writeToSharedPrefs(value: String, key: String) {
+    val sharedPrefs = activity?.getPreferences(Context.MODE_PRIVATE) ?: return
+    with(sharedPrefs.edit()) {
+        putString(key, value)
+        apply()
+    }
+}
+
+fun Fragment.readFromSharedPrefs(key: String): String? {
+    val sharedPrefs = activity?.getPreferences(Context.MODE_PRIVATE) ?: return null
+    return sharedPrefs.getString(key, null)
 }
 
 private fun Context.hideKeyboard(view: View) {
